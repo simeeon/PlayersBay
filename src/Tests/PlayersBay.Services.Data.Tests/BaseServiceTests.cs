e17a92f799aa000a6558cebb864861ea1867912b
simeeon <simeon.t.radev@gmail.com>
2019-01-02 11:15:34 +0200

    Implemented partial GamesServiceTests

---
+++ src/Tests/PlayersBay.Services.Data.Tests/BaseServiceTests.cs
@@ -0,0 +1,96 @@
+﻿namespace PlayersBay.Services.Data.Tests
+{
+    using System;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.DependencyInjection;
+    using CloudinaryDotNet;
+    using PlayersBay.Data;
+    using PlayersBay.Data.Common.Repositories;
+    using PlayersBay.Data.Models;
+    using PlayersBay.Data.Repositories;
+    using PlayersBay.Services.Data.Contracts;
+    using PlayersBay.Services.Data;
+    using PlayersBay.Services.Mapping;
+    using PlayersBay.Services.Data.Models.Games;
+    using System.Reflection;
+    using PlayersBay.Web.ViewModels.Account;
+
+    public abstract class BaseServiceTests : IDisposable
+    {
+        protected BaseServiceTests()
+        {
+            var services = this.SetServices();
+
+            this.ServiceProvider = services.BuildServiceProvider();
+            this.DbContext = this.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        }
+
+        protected IServiceProvider ServiceProvider { get; set; }
+
+        protected ApplicationDbContext DbContext { get; set; }
+
+        private ServiceCollection SetServices()
+        {
+            var services = new ServiceCollection();
+
+            services.AddDbContext<ApplicationDbContext>(
+                opt => opt.UseInMemoryDatabase(Guid.NewGuid().ToString()));
+
+            services
+                    .AddIdentity<ApplicationUser, ApplicationRole>(options =>
+                    {
+                        options.Password.RequireDigit = false;
+                        options.Password.RequireLowercase = false;
+                        options.Password.RequireUppercase = false;
+                        options.Password.RequireNonAlphanumeric = false;
+                        options.Password.RequiredLength = 6;
+                    })
+                    .AddEntityFrameworkStores<ApplicationDbContext>()
+                    .AddUserStore<ApplicationUserStore>()
+                    .AddRoleStore<ApplicationRoleStore>()
+                    .AddDefaultTokenProviders();
+
+            // Data repositories
+            services.AddScoped(typeof(IDeletableEntityRepository<>), typeof(EfDeletableEntityRepository<>));
+            services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
+
+            // Application services
+            services.AddScoped<IGamesService, GamesService>();
+            services.AddScoped<IOffersService, OffersService>();
+            services.AddScoped<IMessagesService, MessagesService>();
+            services.AddScoped<IUsersService, UsersService>();
+            services.AddScoped<ITransactionsService, TransactionsService>();
+            services.AddScoped<IFeedbacksService, FeedbacksService>();
+
+            // Identity stores
+            services.AddTransient<IUserStore<ApplicationUser>, ApplicationUserStore>();
+            services.AddTransient<IRoleStore<ApplicationRole>, ApplicationRoleStore>();
+
+            // AutoMapper
+            AutoMapper.Mapper.Reset();
+            AutoMapperConfig.RegisterMappings(typeof(LoginViewModel).GetTypeInfo().Assembly);
+
+            // Cloudinary Setup
+            var cloudinaryAccount = new Account(
+                "playersbay",
+                "786522667459557",
+                "4dbF6YcUWfHcADOxztDfAyzxxDI");
+            var cloudinary = new Cloudinary(cloudinaryAccount);
+
+            services.AddSingleton(cloudinary);
+
+            var context = new DefaultHttpContext();
+            services.AddSingleton<IHttpContextAccessor>(new HttpContextAccessor { HttpContext = context });
+
+            return services;
+        }
+
+        public void Dispose()
+        {
+            DbContext.Database.EnsureDeleted();
+            this.SetServices();
+        }
+    }
+}
---
+++ src/Tests/PlayersBay.Services.Data.Tests/GamesServiceTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PlayersBay.Data.Models;
+using PlayersBay.Services.Data.Contracts;
+using PlayersBay.Services.Data.Models.Games;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PlayersBay.Services.Data.Tests
+{
+    public class GamesServiceTests : BaseServiceTests
+    {
+        private const string Diablo = "Diablo";
+        private const string CsGo = "CsGo";
+        private const string Minecraft = "Minecraft";
+        private const string DefaultImage = "http://icons.iconarchive.com/icons/guillendesign/variations-3/256/Default-Icon-icon.png"; 
+
+        private IGamesService GamesServiceMock => this.ServiceProvider.GetRequiredService<IGamesService>();
+
+        [Fact]
+        public async Task GetAllAsyncReturnsAllGames()
+        {
+            this.DbContext.Games.Add(new Game { Id = 1, Name = Diablo, ImageUrl = DefaultImage });
+            this.DbContext.Games.Add(new Game { Id = 2, Name = CsGo, ImageUrl = DefaultImage });
+            this.DbContext.Games.Add(new Game { Id = 3, Name = Minecraft, ImageUrl = DefaultImage });
+            await this.DbContext.SaveChangesAsync();
+
+            var expected = new GameViewModel[]
+            {
+                new GameViewModel {Id = 1, Name = Diablo, ImageUrl = DefaultImage},
+                new GameViewModel {Id = 2, Name = CsGo, ImageUrl = DefaultImage},
+                new GameViewModel {Id = 3, Name = Minecraft, ImageUrl = DefaultImage},
+            };
+
+            var actual = await this.GamesServiceMock.GetAllGamesAsync();
+
+            Assert.Collection(actual,
+                elem1 =>
+                {
+                    Assert.Equal(expected[0].Id, elem1.Id);
+                    Assert.Equal(expected[0].Name, elem1.Name);
+                    Assert.Equal(expected[0].ImageUrl, elem1.ImageUrl);
+                },
+                elem2 =>
+                {
+                    Assert.Equal(expected[1].Id, elem2.Id);
+                    Assert.Equal(expected[1].Name, elem2.Name);
+                    Assert.Equal(expected[1].ImageUrl, elem2.ImageUrl);
+                },
+                elem3 =>
+                {
+                    Assert.Equal(expected[2].Id, elem3.Id);
+                    Assert.Equal(expected[2].Name, elem3.Name);
+                    Assert.Equal(expected[2].ImageUrl, elem3.ImageUrl);
+                });
+            Assert.Equal(expected.Length, actual.Length);
+        }
+    }
+}
